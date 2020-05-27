9b7996bd51a9a32e003bd7a27ffe42f71b3a45e9
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-27 10:15:24 -0600

    Add museum tests

---
+++ test/museum_test.rb
@@ -0,0 +1,53 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "mocha/minitest"
+require "./lib/patron"
+require "./lib/exhibit"
+require "./lib/museum"
+
+class MuseumTest < Minitest::Test
+
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    @patron_1 = Patron.new("Bob", 20)
+    @patron_2 = Patron.new("Sally", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_it_can_recommend_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    @patron_2.add_interest("IMAX")
+
+    @dmns.recommend_exhibits(@patron_1)
+
+    assert_equal [@dead_sea_scrolls, @gems_and_minerals], @dmns.recommend_exhibits
+
+    @dmns.recommend_exhibits(@patron_2)
+    assert_equal [@imax], @dmns.recommend_exhibits
+  end
+end
